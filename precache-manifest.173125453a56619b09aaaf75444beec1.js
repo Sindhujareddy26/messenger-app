b0ee0f0fecdf15f05b465375eabb4547e48c00d5
sindhu <sindhujareddy2610@gmail.com>
2020-09-05 11:27:44 +0530

    Updates

---
+++ precache-manifest.173125453a56619b09aaaf75444beec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00cea2a83054a79d63e809b6d1f3eb5",
+    "url": "/messenger-app/index.html"
+  },
+  {
+    "revision": "df5b5635656345ed80a4",
+    "url": "/messenger-app/static/css/main.eb918993.chunk.css"
+  },
+  {
+    "revision": "690e58c4ba018fdfc754",
+    "url": "/messenger-app/static/js/2.b0adc719.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/messenger-app/static/js/2.b0adc719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5b5635656345ed80a4",
+    "url": "/messenger-app/static/js/main.0b778b56.chunk.js"
+  },
+  {
+    "revision": "05fad1f276d0aeb46e2d",
+    "url": "/messenger-app/static/js/runtime-main.297f7124.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/messenger-app.gitp/precache-manifest.a248d4802350eb7dbdcebdc0a48e0c44.js"
+  "/messenger-app/precache-manifest.173125453a56619b09aaaf75444beec1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/messenger-app.gitp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/messenger-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
