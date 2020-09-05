19e7541d66b036c5689dbf6e7bde8c30cc1b2a0c
sindhu <sindhujareddy2610@gmail.com>
2020-09-05 11:10:42 +0530

    Updates

---
+++ precache-manifest.8c665fc73055bc65700790105a8f6e58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44825b3ce193f3e338a0ed16987efde8",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "fc0d04eb90778eb27fb1",
+    "url": "/react-gh-pages/static/css/main.eb918993.chunk.css"
+  },
+  {
+    "revision": "e1875fb0f6f7c2deab51",
+    "url": "/react-gh-pages/static/js/2.2c7f98a7.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/react-gh-pages/static/js/2.2c7f98a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc0d04eb90778eb27fb1",
+    "url": "/react-gh-pages/static/js/main.5729f5f8.chunk.js"
+  },
+  {
+    "revision": "1caab8dc5f48a18416c9",
+    "url": "/react-gh-pages/static/js/runtime-main.08eec64a.js"
+  }
+]);
